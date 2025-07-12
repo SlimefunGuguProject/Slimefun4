7cd0a8f18fcbfa76413dfaa7279861593cd57cba
StarWishsama <noraincity25@gmail.com>
2025-07-12 21:33:32 +0800

    fix(task): `TaskQueue` should handle runAtLoc/Entity properly

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/blocks/Composter.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/blocks/Composter.java
@@ -1,7 +1,7 @@
 package io.github.thebusybiscuit.slimefun4.implementation.items.blocks;
 
+import city.norain.slimefun4.dough.TaskQueue;
 import io.github.bakedlibs.dough.protection.Interaction;
-import io.github.bakedlibs.dough.scheduling.TaskQueue;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
@@ -87,15 +87,23 @@ public class Composter extends SimpleSlimefunItem<BlockUseHandler> implements Re
                     if (output != null) {
                         TaskQueue tasks = new TaskQueue();
 
-                        tasks.thenRepeatEvery(30, 10, () -> {
-                            Material material = input.getType().isBlock() ? input.getType() : Material.HAY_BLOCK;
-                            b.getWorld().playEffect(b.getLocation(), Effect.STEP_SOUND, material);
-                        });
+                        tasks.thenRepeatEvery(
+                                30,
+                                10,
+                                () -> {
+                                    Material material =
+                                            input.getType().isBlock() ? input.getType() : Material.HAY_BLOCK;
+                                    b.getWorld().playEffect(b.getLocation(), Effect.STEP_SOUND, material);
+                                },
+                                b.getLocation());
 
-                        tasks.thenRun(20, () -> {
-                            SoundEffect.COMPOSTER_COMPOST_SOUND.playFor(p);
-                            pushItem(b, output.clone());
-                        });
+                        tasks.thenRun(
+                                20,
+                                () -> {
+                                    SoundEffect.COMPOSTER_COMPOST_SOUND.playFor(p);
+                                    pushItem(b, output.clone());
+                                },
+                                b.getLocation());
 
                         tasks.execute(Slimefun.instance());
                     } else {
